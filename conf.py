10020b654701ae10355968725c2473be0c1f71cc
Levi Armstrong <levi.armstrong@gmail.com>
2017-10-04 12:55:15 -0500

    fixup

--- conf.py
+++ conf.py
@@ -53,7 +53,9 @@ project = u'Industrial Training'
 copyright = u'2017, ROS-Industrial'
 
 # Subversioning Settings
+scv_show_banner = True
 scv_banner_greatest_tag = True
+
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
